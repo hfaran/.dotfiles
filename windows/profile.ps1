c1cfe12e0fa190fcb28078dad0b807e837043968
hfaran <hamzafaran@hotmail.com>
2014-11-29 23:25:21 -0800

    Add powershell profile

---
+++ windows/profile.ps1
@@ -0,0 +1,7 @@
+# Ensure that $profile sources this file by appending:
+# `. C:\Users\Hamza\profile.ps1` or wherever this file lives
+# to the file which as $profile
+
+
+Function gs {git status $args}
+Function gd {git diff $args}
