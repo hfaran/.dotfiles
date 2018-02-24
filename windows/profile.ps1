8a4596b6ec160ea178d180f0aec8e7707654cbeb
Hamza Faran <hfaran@users.noreply.github.com>
2018-02-23 21:48:18 -0800

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -7,7 +7,7 @@
 # Imports #
 ###########
 
-$env:PSModulePath = $env:PSModulePath + ";E:\Documents\WindowsPowerShell\modules"
+$env:PSModulePath = $env:PSModulePath # + ";E:\Documents\WindowsPowerShell\modules"
 
 Import-Module PSReadLine
 Import-Module z
@@ -24,12 +24,12 @@ try { $null = gcm pshazz -ea stop; pshazz init } catch { }
 # Set "Menu" (bash style) completion with Tab key rather than Ctrl-Space; https://github.com/lzybkr/PSReadLine
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
-# Persistent History setup; https://software.intel.com/en-us/blogs/2014/06/17/giving-powershell-a-persistent-history-of-commands
-$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
-Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
-If (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+# # Persistent History setup; https://software.intel.com/en-us/blogs/2014/06/17/giving-powershell-a-persistent-history-of-commands
+# $HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
+# Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
+# If (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
+# Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 
 ###########
